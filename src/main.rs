4b01ab14b7aa20d4ab677ae17cd470740108ad32
Gaßmann, Aaron <Aaron.Gassmann@dts.de>
2025-05-08 10:27:58 +0200

    remove service

--- src/main.rs
+++ src/main.rs
@@ -14,11 +14,6 @@ async fn greet(name: actix_web::web::Path<String>) -> impl Responder {
     HttpResponse::Ok().body(format!("Hello {}!", name.into_inner()))
 }
 
-#[get("/fuck/{name}")]
-async fn fuck(name: actix_web::web::Path<String>) -> impl Responder {
-    HttpResponse::Ok().body(format!("Fuck you {}!", name.into_inner()))
-}
-
 // The main function where the server is set up and started
 #[actix_web::main] // Marks the main function to be run by the Actix system
 async fn main() -> std::io::Result<()> {
@@ -28,7 +23,6 @@ async fn main() -> std::io::Result<()> {
         App::new()
             .service(hello)
             .service(greet)
-            .service(fuck)
     })
     // Bind the server to the address 127.0.0.1:8080
     .bind("127.0.0.1:8080")?
